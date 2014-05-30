ddd7fccac377aefea2e51e36188ae6d8342e4f7d
Patrick McGraw <patrick@mcgraw-tech.com>
2014-05-30 09:35:07 -0700

    Adding the capability to ship files to rackspace in addition to aws

--- lib/pgbackups-archive/storage.rb
+++ lib/pgbackups-archive/storage.rb
@@ -26,6 +26,32 @@ class PgbackupsArchive::Storage
   def store
     puts "Begin file upload [#{@file}]"
     bucket.files.create :key => @key, :body => @file, :public => false, :multipart_chunk_size => 5242880
+
+    if rackspace_directory = get_rackspace_directory
+      puts "Begin RACKSPACE file upload [#{@file}]"
+      rackspace_directory.files.create(:key => @key, :body => @file, :multipart_chunk_size => 5242880)
+    end
+
+  end
+
+  def get_rackspace_directory
+    return nil unless api = connect_rackspace
+
+    puts "Open Rackspace directory: #{ENV['RACKSPACE_CONTAINER_NAME']}"
+    dirs = api.directories
+    dir = dirs.select { |d| d.key == ENV['RACKSPACE_CONTAINER_NAME'] }.last
+  end
+
+  def connect_rackspace
+    return nil unless ENV['RACKSPACE_USER_NAME'] and ENV['RACKSPACE_API'] and ENV['RACKSPACE_CONTAINER_NAME']
+
+    service = Fog::Storage.new({
+        :provider            => 'Rackspace',         # Rackspace Fog provider
+        :rackspace_username  => ENV['RACKSPACE_USER_NAME'], # Your Rackspace Username
+        :rackspace_api_key   => ENV['RACKSPACE_API'],       # Your Rackspace API key
+        :rackspace_region    => :ord,                # Defaults to :dfw
+        :connection_options  => {}                   # Optional
+    })
   end
 
 end
